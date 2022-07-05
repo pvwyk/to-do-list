de671d597e78aff70409f55859310cef644efb69
pvwyk <102585283+pvwyk@users.noreply.github.com>
2022-07-05 15:24:04 +0200

    First commit
    First commit for my to do list app

---
+++ index.js
@@ -0,0 +1,167 @@
+let todoItems = [];
+
+function renderTodo(todo) {
+  localStorage.setItem("todoItems", JSON.stringify(todoItems));
+
+  const list = document.querySelector(".js-todo-list");
+  const item = document.querySelector(`[data-key='${todo.id}']`);
+
+  if (todo.deleted) {
+    item.remove();
+    if (todoItems.length === 0) list.innerHTML = "";
+    return;
+  }
+
+  const isChecked = todo.checked ? "done" : "";
+  const node = document.createElement("li");
+  node.setAttribute("class", `todo-item ${isChecked}`);
+  node.setAttribute("data-key", todo.id);
+  node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  `;
+
+  if (item) {
+    list.replaceChild(node, item);
+  } else {
+    list.append(node);
+  }
+  taskAmount();
+}
+
+function addTodo(text) {
+  const todo = {
+    text,
+    checked: false,
+    id: Date.now(),
+  };
+
+  todoItems.push(todo);
+  renderTodo(todo);
+}
+
+function toggleDone(key) {
+  const index = todoItems.findIndex((item) => item.id === Number(key));
+  todoItems[index].checked = !todoItems[index].checked;
+
+  renderTodo(todoItems[index]);
+  localStorage.setItem("todoItems", JSON.stringify(todoItems));
+}
+
+function deleteTodo(key) {
+  const index = todoItems.findIndex((item) => item.id === Number(key));
+  const todo = {
+    deleted: true,
+    ...todoItems[index],
+  };
+  todo.deleted = true;
+  todoItems = todoItems.filter((item) => item.id !== Number(key));
+  renderTodo(todo);
+}
+
+const form = document.querySelector(".js-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = document.querySelector(".js-todo-input");
+
+  const text = input.value.trim();
+  if (text !== "") {
+    addTodo(text);
+    input.value = "";
+    input.focus();
+  }
+});
+
+const list = document.querySelector(".js-todo-list");
+list.addEventListener("click", (event) => {
+  if (event.target.classList.contains("js-tick")) {
+    const itemKey = event.target.parentElement.dataset.key;
+    toggleDone(itemKey);
+  }
+
+  if (event.target.classList.contains("js-delete-todo")) {
+    const itemKey = event.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ref = localStorage.getItem("todoItems");
+  if (ref) {
+    todoItems = JSON.parse(ref);
+    todoItems.forEach((t) => {
+      renderTodo(t);
+    });
+  }
+});
+
+const weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const d = new Date();
+let day = weekday[d.getDay()];
+//document.getElementById("date").innerHTML = day;
+
+const nth = function (d) {
+  if (d > 3 && d < 21) return "th";
+  switch (d % 10) {
+    case 1:
+      return "st";
+    case 2:
+      return "nd";
+    case 3:
+      return "rd";
+    default:
+      return "th";
+  }
+};
+
+// test code
+
+const fortnightAway = new Date(+new Date() + 12096e5);
+const date = fortnightAway.getDate();
+const month = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+][fortnightAway.getMonth()];
+
+document.getElementById("date1").innerHTML = `${day},`;
+
+document.getElementById("date2").innerHTML = `${date}<sup>${nth(date)}</sup> `;
+
+document.getElementById("date3").innerHTML = `${month}`;
+
+// test
+const dates = [...Array(32).keys()].slice(1).map((i) => `${i}${nth(i)}`);
+console.log(dates.join(", "));
+
+//number of tasks
+function taskAmount() {
+  let totalTasks = todoItems.length;
+  document.getElementById("item-list").innerHTML = `${totalTasks}`;
+  //let checkedTasks = document.getElementById("item-list") = `${totalTasks}`
+
+  console.log(totalTasks);
+  //   console.log(checkedTasks);
+}
