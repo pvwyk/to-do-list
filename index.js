753906a10f99454d3999368c21939dc646e4203a
pvwyk <102585283+pvwyk@users.noreply.github.com>
2022-07-06 16:32:41 +0200

    Commited to winnig
    wassuuuuup

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function renderTodo(todo) {
   node.innerHTML = `
     <input id="${todo.id}" type="checkbox"/>
     <label for="${todo.id}" class="tick js-tick"></label>
-    <span>${todo.text}</span>
+    <span class="span2">${todo.text}</span>
     <button class="delete-todo js-delete-todo">
     <svg><use href="#delete-icon"></use></svg>
     </button>
@@ -31,6 +31,48 @@ function renderTodo(todo) {
     list.append(node);
   }
   taskAmount();
+  let spanList = Array.from(document.getElementsByClassName("span2"));
+
+  spanList.forEach((box, i) => {
+    box.addEventListener("dblclick", function handleClick(event) {
+      const modal2 = document.getElementById("myModal2");
+      console.log(modal2);
+      console.log(i);
+
+      let getEventId = i;
+      modal2.style.display = "block";
+
+      itemClicked = event;
+      console.log("item clicked", event);
+      let e = event.target.childNodes[0].data;
+      console.log(e);
+      updateUi(event, getEventId);
+      //event.target.childNodes[0].data = "testing";
+
+      //console.log(event.target.childNodes[0].data);
+
+      // form.value = e.parentElement.previousElementSibling.innerHTML;
+      // e.parentElement.parentElement.remove();
+      //open modal
+      //edit data
+    });
+  });
+}
+
+//function updateLi()
+function updateToDo(index, newValue) {
+  // todoItems = localStorage.getItem("todoItems");
+
+  // todoItems[index].text = newValue;
+  // todoItems[index].id = Date.now();
+  const todo = {
+    text: newValue,
+    checked: false,
+    id: Date.now(),
+  };
+  console.log("todoItems updates", todoItems[index]);
+  todoItems[index] = todo;
+  renderTodo(todoItems[index]);
 }
 
 function addTodo(text) {
@@ -41,6 +83,7 @@ function addTodo(text) {
   };
 
   todoItems.push(todo);
+  // updateToDo(1, "tester1234");
   renderTodo(todo);
 }
 
@@ -65,6 +108,7 @@ function deleteTodo(key) {
 
 const form = document.querySelector(".js-form");
 form.addEventListener("submit", (event) => {
+  modal.style.display = "none";
   event.preventDefault();
   const input = document.querySelector(".js-todo-input");
 
@@ -99,6 +143,57 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+//Double click to update
+
+// let uPtodo = document.getElementById("update-to-do");
+// uPtodo.addEventListener("click", updateToDo);
+
+//get dblclick id
+
+let modal2 = document.getElementById("myModal2");
+
+function updateUi(event, getEventId) {
+  //get modal 2
+  // let modal2 = document.getElementById("myModal2");
+
+  let event_id = getEventId;
+
+  // Get the button that opens the modal
+  let btn2 = document.getElementById("myBtn2");
+
+  // Get the <span> element that closes the modal
+  let span2 = document.getElementsByClassName("close")[0];
+  let modalInput = document.getElementsByClassName("js-todo-input2");
+  console.log(modalInput);
+  modalInput[0].value = event.target.childNodes[0].data;
+  // modalInput[0].text = event.target.childNodes[0].data;
+
+  // updateToDo(2, modalInput[0].text);
+
+  // When the user clicks on the element, open the modal
+
+  // modal2.style.display = "block";
+
+  // When the user clicks on <span> (x), close the modal
+
+  const form2 = document.querySelector(".update-form");
+  form2.addEventListener("submit", (event) => {
+    modal2.style.display = "none";
+    // event.preventDefault();
+    const input = document.querySelector(".js-todo-input2");
+    modal.style.display = "none";
+    const text = input.value.trim();
+    console.log(text);
+    if (text !== "") {
+      updateToDo(event_id, text);
+      console.log("Done with updateToDo");
+      input.value = "";
+      input.focus();
+    }
+  });
+}
+
+//get the day
 const weekday = [
   "Sunday",
   "Monday",
@@ -113,6 +208,7 @@ const d = new Date();
 let day = weekday[d.getDay()];
 //document.getElementById("date").innerHTML = day;
 
+//th, nd and so on
 const nth = function (d) {
   if (d > 3 && d < 21) return "th";
   switch (d % 10) {
@@ -129,8 +225,11 @@ const nth = function (d) {
 
 // test code
 
-const fortnightAway = new Date(+new Date() + 12096e5);
+// const fortnightAway = new Date(+new Date() + 12096e5);
+// const date = fortnightAway.getDate();
+const fortnightAway = new Date();
 const date = fortnightAway.getDate();
+
 const month = [
   "January",
   "February",
@@ -147,21 +246,52 @@ const month = [
 ][fortnightAway.getMonth()];
 
 document.getElementById("date1").innerHTML = `${day},`;
-
 document.getElementById("date2").innerHTML = `${date}<sup>${nth(date)}</sup> `;
-
 document.getElementById("date3").innerHTML = `${month}`;
 
 // test
 const dates = [...Array(32).keys()].slice(1).map((i) => `${i}${nth(i)}`);
-console.log(dates.join(", "));
+//console.log(dates.join(", "));
 
 //number of tasks
 function taskAmount() {
   let totalTasks = todoItems.length;
-  document.getElementById("item-list").innerHTML = `${totalTasks}`;
+  document.getElementById("item-list").innerHTML = `${totalTasks} Tasks`;
   //let checkedTasks = document.getElementById("item-list") = `${totalTasks}`
 
   console.log(totalTasks);
   //   console.log(checkedTasks);
 }
+
+// Get the modal 1
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+};
